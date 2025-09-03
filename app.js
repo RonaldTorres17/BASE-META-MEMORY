751ce1c534704f58b83c0b7c0bdccd7884e5bd86
RonaldTorres17 <ronaldsanchez@upeu.edu.pe>
2025-09-03 15:07:18 -0500

    Express added.6

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ const main = async () => {
   ]);
 
   const adapterProvider = createProvider(MetaProvider, {
-    jwtToken: 'EAAXpk12nUvMBPdsn4AHwC8SwtlYDu6cIyaYxmIrPkaapaOpZCY9tRKZBBc5QTJI4WzKAIKTwgNfKEYVFkpeTro17NP24vD3Xuh8WjDIioZAsF9ZAlqlvcUP47lJyatoMj6MTO22qPw3O7JjVEFKJ3yEZCoJCIaEiAr2GGLC86fsAUcC77bMsahyW83nZAc7PaEZCdnFuBKwONhJQ6VJ36oVpbXFlh65Waza2L1jOIhUBHUkTQZDZD',
-    numberId: 'T791792920678436',
+    jwtToken: 'EAAXpk12nUvMBPbKJeg2La7DrSFf5eqvyMJOoz9uVEGbEH3fcpuE4gDgZBa0YusPLlwTCKzrxckqfTZAj8ZBLMrXPKBB19LsFDLf97oXZAGLUwSGPFlYZBRhGQ2BmnDP6p4fHGi2jS0KonORVrwcs8NyKBNR0xE6XxW06b4S3r5b2OMwQPU76wUmBWPIn8FZAZAtLZClB65eZAGeZBf1rZCgCXZBa1RY0fm5xkvvOo0VWQgJuuVTVnAZDZD',
+    numberId: '791792920678436',
     verifyToken: 'verifyTokenPrueba',
     version: 'v23.0',
     baseUrl: `https://base-meta-memory-r1d6.onrender.com` // 👈 tu URL pública en Render
